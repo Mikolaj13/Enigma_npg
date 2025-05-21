d71957909ab5a2c5fbf62d535ba8acdc248c8fc1
Mikolaj Dworak <mikolajdworak6@gmail.com>
2025-05-21 16:20:30 +0200

    Redesign of RotorAndRotorListModule.py to take advantage of inharitence

--- AdditionalModules/RotorAndRotorListModule.py
+++ AdditionalModules/RotorAndRotorListModule.py
@@ -6,51 +6,61 @@ class Rotor:
     def __getitem__(self, key):
         return self.rotor_dictionary[key]
 
-    def rotation_values(self):
+    def rotation(self):
+        pass
+
+    def setting(self,s):
+        pass
+
+
+class Encrypting_Rotor(Rotor):
+    def rotation(self):
         keys = list(self.rotor_dictionary.keys())
         values = list(self.rotor_dictionary.values())
         self.setting += 1
         self.setting %= 26
         self.rotor_dictionary=dict(zip(keys, values[1:]+[values[0]]))
-
-    def setting_values(self,s):
+    def setting(self,s):
         for i in range(0,s):
-            self.rotation_values()
+            self.rotation()
 
-    def rotation_keys(self):
+class Decrypting_Rotor(Rotor):
+    def rotation(self):
         keys = list(self.rotor_dictionary.keys())
         values = list(self.rotor_dictionary.values())
         self.setting += 1
         self.setting %= 26
-        self.rotor_dictionary=dict(zip(keys[1:]+[keys[0]], values))
-
-    def setting_keys(self,s):
-        for i in range(0,s):
-            self.rotation_keys()
-
+        self.rotor_dictionary = dict(zip(keys[1:] + [keys[0]], values))
+    def setting(self,s):
+        for i in range(0, s):
+            self.rotation()
 
 class Rotor_Set:
-    def __init__(self,rotor_list, index):
+    def __init__(self, rotor_list, setting=0):
         self.rotor_list = rotor_list
-        self.index = index
+        self.setting = setting
 
     def __getitem__(self, key):
         for i in self.rotor_list:
             key = i[key]
 
-    def turn_values(self):
-        self.rotor_list[self.index%len(self.rotor_list)].rotation_values()
+    def turn(self):
+        pass
 
-    def turn_keys(self):
+class Encrypting_Rotor_Set(Rotor_Set):
+    def turn(self):
+        self.rotor_list[self.setting%len(self.rotor_list)].rotation_values()
+        self.setting+=1
+
+class Decrypting_Rotor_Set(Rotor_Set):
+    def turn(self):
         a = {0:2,1:1,2:0}
-        self.rotor_list[a[self.index%len(self.rotor_list)]].rotation_keys()
+        self.rotor_list[a[self.setting%len(self.rotor_list)]].rotation_keys()
+        self.setting+=1
 
+class Enigma_Engine:
+    def __init__(self,rotor_dict_list):
+        self.e_rotor_set = Encrypting_Rotor_Set([Encrypting_Rotor(i) for i in rotor_dict_list])
+        self.d_rotor_set = Decrypting_Rotor_Set([Decrypting_Rotor(dict(zip(i.values,i.keys))) for i in rotor_dict_list])
     def encryption(self, character):
-        self.turn_values()
-        self.index+=1
-        return self[character]
-
-    def decryption(self, character):
-        self.turn_keys()
-        self.index+=1
-        return self[character]
+        pass
---
+++ AdditionalModules/test_RotorAndRotorListModule.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestRotor_Set(TestCase):
+    def test_encryption(self):
+        
+        self.fail()
