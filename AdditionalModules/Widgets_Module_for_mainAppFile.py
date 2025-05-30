f4256371317d4ced740c1af5ce257ea4205e315a
Mikolaj Dworak <mikolajdworak6@gmail.com>
2025-05-30 13:47:31 +0200

    Zmiana layoutu i kilka poprawek

--- AdditionalModules/Widgets_Module_for_mainAppFile.py
+++ AdditionalModules/Widgets_Module_for_mainAppFile.py
@@ -1,27 +1,27 @@
 from tkinter import *
 
-class ButtonIOMaker():
-    def __init__(self, row, col, text, window):
+class ButtonIOMaker:
+    def __init__(self, row, col, text, window,colour):
         self.window = window
-
-        self.button = Button(window,width=2, height=4 , text=text, bg="light grey")
-        self.button.grid(row=row, column=col, rowspan=3)
+        self.colour = colour
+        self.button = Button(window,width=4, height=2 , text=text, bg="light grey")
+        self.button.grid(row=row, column=col)
         self.button.config(state=DISABLED)
 
     def clicked(self):
-        self.button.config(bg="green")
+        self.button.config(bg=self.colour)
         self.window.after(200, lambda: self.button.config(bg="light grey"))
 
-class TextMaker():
-    def __init__(self, row, window, text):
-        Label(window, text= text).grid(row=row,columnspan=2)
-        self.text = Text(window, height=8,width = 80)
+class TextMaker:
+    def __init__(self, row,col, window, text):
+        Label(window, text= text).grid(row=row,column = col)
+        self.text = Text(window, height=8,width = 40)
 
         scroll = Scrollbar(window, command=self.text.yview)
         self.text.configure(yscrollcommand=scroll.set)
 
-        self.text.grid(row=row+1, column=0,columnspan=25, sticky="nsew")
-        scroll.grid(row=row+1, column=25, sticky="ns")
+        self.text.grid(row=row+1, column=col,columnspan=12, sticky="nsew")
+        scroll.grid(row=row+1, column=col+13, sticky="ns")
 
         self.text.config(state=DISABLED)
 
@@ -34,3 +34,24 @@ class TextMaker():
         self.text.config(state=NORMAL)
         self.text.delete("end-2c", "end-1c")
         self.text.config(state=DISABLED)
+
+    def getlength(self):
+        return len(self.text.get("1.0", "end-1c"))
+    def getlastletter(self):
+        return self.text.get("1.0", "end-1c")[-1]
+
+
+class TextIOPair:
+    def __init__(self, row, window):
+        self.input_text_field = TextMaker(row, 0, window, "Input")
+        self.output_text_field = TextMaker(row, 14, window, "Output")
+    def typing(self,character_in,character_out):
+        self.input_text_field.typing(character_in)
+        self.output_text_field.typing(character_out)
+    def backspace(self):
+        self.input_text_field.backspace()
+        self.output_text_field.backspace()
+    def getlength(self):
+        return self.input_text_field.getlength()
+    def getlastletter(self):
+        return self.input_text_field.getlastletter()
--- mainAppFile.py
+++ mainAppFile.py
@@ -15,36 +15,30 @@ class EnigmaApp(Tk):
 
         self.title("Enigma")
 
-        self.input_text_field = Widgets.TextMaker(2, self, "Input")
-        self.output_text_field = Widgets.TextMaker(4, self, "Output")
+        self.IO_text_fields = Widgets.TextIOPair(10,self)
 
+        Label().grid(row=4, column=0) # wypełnienie między klawiaturami
         self.list_of_output_buttons = list()
         self.list_of_input_buttons = list()
 
-        for i, l in enumerate(string.ascii_uppercase):
-            row = 6
-            col = i
-            self.list_of_input_buttons.append(Widgets.ButtonIOMaker(row, col, l, self))
-            self.list_of_output_buttons.append(Widgets.ButtonIOMaker(row + 3, col, l, self))
+        for i, j in enumerate(string.ascii_uppercase):
+            if i < 20:
+                self.list_of_output_buttons.append(Widgets.ButtonIOMaker((i//10),i%10,j,self,"white"))
+                self.list_of_input_buttons.append(Widgets.ButtonIOMaker((i // 10)+5, i % 10, j, self,"grey"))
+            else:
+                self.list_of_output_buttons.append(Widgets.ButtonIOMaker((i//10),(i%10)+2,j,self,"white"))
+                self.list_of_input_buttons.append(Widgets.ButtonIOMaker((i // 10)+5, (i % 10) + 2, j, self,"grey"))
 
     def on_key_press(self,event):
 
         character = event.char.upper()
-        print(character)
-
-        if event.keysym == "BackSpace" and  len(self.input_text_field.text.get("1.0", "end-1c")) >0:
-            text = self.input_text_field.text.get("1.0", "end-1c")
-
-            self.input_text_field.backspace()
-            self.output_text_field.backspace()
-
-            if text[-1] in string.ascii_uppercase:
+        if event.keysym == "BackSpace" and  self.IO_text_fields.getlength() >0:
+            if self.IO_text_fields.getlastletter() in string.ascii_uppercase:
                 self.enigma_engine.backspace()
+            self.IO_text_fields.backspace()
 
         if character in SIGN_LIST:
-            self.input_text_field.typing(character)
-
-            self.output_text_field.typing(character)
+            self.IO_text_fields.typing(character,character)
 
         if character in string.ascii_uppercase:
 
@@ -53,12 +47,9 @@ class EnigmaApp(Tk):
             else:
                 character_after = self.enigma_engine.decryption(character)
 
-            self.input_text_field.typing(character)
-
-            self.output_text_field.typing(character_after)
+            self.IO_text_fields.typing(character,character_after)
 
             self.list_of_input_buttons[string.ascii_uppercase.index(character)].clicked()
-
             self.list_of_output_buttons[string.ascii_uppercase.index(character_after)].clicked()
 
 
