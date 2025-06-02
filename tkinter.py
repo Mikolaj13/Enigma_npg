4a2aaacb14784cfde831fc7173ab78c5f5c6f920
DawidG-n <dawidgiel@student.agh.edu.pl>
2025-06-02 21:40:12 +0200

    Update tkinter.py
    Opracowanie ko końcowego zarysu klasy setting

--- tkinter.py
+++ tkinter.py
@@ -1,1 +1,13 @@
+from tkinter import *
+#Zarys kalsy setting
+class SettingApp(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Enigma Rotor Settings")
 
+        self.rotor_count = 3
+        self.widget_list = WidgetSettingPackList(self)
+        Button(text = "add a rotor",width=12,height=2,command=lambda: self.widget_list.add()).grid(column=4, row=0)
+        Button(text="subtract a rotor", width=12, height=2, command=lambda: self.widget_list.sub()).grid(column=4, row=1)
+        Button(text="reset", width=12, height=2, command=lambda: self.widget_list.reset()).grid(column=5, row=0)
+        Button(text="continue", width=12, height=2, command=lambda: self.cont()).grid(column=5, row=1)
